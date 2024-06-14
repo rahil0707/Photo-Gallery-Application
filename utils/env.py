13284c9dd5b4265771af80655cd4b04b008be952
rahil0707 <rahilhussain10@gmail.com>
2024-06-14 17:56:19 -0400

    Uploaded utils file and pem key

---
+++ utils/env.py
@@ -0,0 +1,17 @@
+# IAM User access configuration
+AWS_ACCESS_KEY="AKIATZZOWAPNBXUIOMQ2"
+AWS_SECRET_ACCESS_KEY="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"
+AWS_REGION="us-east-1"
+
+# S3 Bucket for photo objects
+PHOTOGALLERY_S3_BUCKET_NAME="photobucket-ece4150-project"
+
+
+# DynamoDB Table
+DYNAMODB_TABLE='HW-Content'
+DYNAMODB_TABLE2='HW-User'
+
+###### INSERT NEW ENVIRONMENT VARIEABLES HERE ######
+
+
+####################################################
---
+++ utils/utils/env.py
@@ -0,0 +1,22 @@
+# IAM User access configuration
+AWS_ACCESS_KEY="AKIATZZOWAPNBXUIOMQ2"
+AWS_SECRET_ACCESS_KEY="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"
+AWS_REGION="us-east-1"
+
+# S3 Bucket for photo objects
+PHOTOGALLERY_S3_BUCKET_NAME="photobucket2-hussain-2022-4150"
+
+# MySQL Configuration
+RDS_DB_HOSTNAME= "photogallerydb.c3ss02vyvyiu.us-east-1.rds.amazonaws.com"
+RDS_DB_USERNAME='root'
+RDS_DB_PASSWORD='Rahil0807$'
+RDS_DB_NAME='photogallerydb'
+
+# DynamoDB Table
+DYNAMODB_TABLE='PhotoGallery2'
+DYNAMODB_TABLE2='PhotoGalleryUser'
+
+###### INSERT NEW ENVIRONMENT VARIEABLES HERE ######
+
+
+####################################################
